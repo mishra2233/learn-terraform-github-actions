aa255a2e26cb9b4d68e152f8f93dcc04fc510eee
mishra2233 <66589335+mishra2233@users.noreply.github.com>
2021-11-15 19:17:09 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -36,7 +36,7 @@ resource "aws_instance" "web" {
 
   user_data = <<-EOF
               #!/bin/bash
-              echo "Hello, Worldssss" > index.html
+              echo "Hello, Worldsssss" > index.html
               nohup busybox httpd -f -p 8080 &
               EOF
 }
